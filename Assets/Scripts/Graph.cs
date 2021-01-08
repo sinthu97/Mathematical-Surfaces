dd0d57bfcfa129b3f7879c018644639c1730c94a
sinthu97 <47841514+sinthu97@users.noreply.github.com>
2021-01-08 10:29:49 +0530

    first commit

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField , Range(10,100)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    Transform[] points;
+
+    void Awake()
+	{
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution * resolution];
+
+        for (int i = 0,x = 0 ,z = 0; i < points.Length; i++,x++) {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            Transform point = Instantiate(pointPrefab);
+            position.x = (x + 0.5f)*step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = f(position.x, position.z, time);
+            point.localPosition = position;
+        }
+
+    }
+}
